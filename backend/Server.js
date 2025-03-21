e624cd8fbae16cd57672806a94740e563733e11c
focusedaf <126765236+focusedaf@users.noreply.github.com>
2025-03-21 16:57:26 +0530

    updates

--- backend/Server.js
+++ backend/Server.js
@@ -1,7 +1,11 @@
-const express = require("express");
+import express from "express";
+import connectDB from "./db/mongodb.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 const port = 3000;
-
+connectDB();
 app.get("/", (req, res) => {
   res.send("Welcome to Echo");
 });
---
+++ backend/models/user.models.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
